d00c259c09fbb9c17ad57ce8d5da57abfa699cd0
eyu-dev <169823478+eyu-dev@users.noreply.github.com>
2024-06-04 17:20:32 +0300

    Initial commit
    Signed-off-by: eyu-dev <169823478+eyu-dev@users.noreply.github.com>

---
+++ Config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDKHTTfLzyIfgImEn_CiLqHwuXK2sEw66c",
+  authDomain: "telebirrteamd5.firebaseapp.com",
+  databaseURL: "https://telebirrteamd5-default-rtdb.firebaseio.com",
+  projectId: "telebirrteamd5",
+  storageBucket: "telebirrteamd5.appspot.com",
+  messagingSenderId: "886900364913",
+  appId: "1:886900364913:web:cf30e08eb1be3030adedd0",
+  measurementId: "G-JYQW6VKWF0"
+};
